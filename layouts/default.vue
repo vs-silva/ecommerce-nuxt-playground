9dde5e09a43299d029d91a2dc674dbd8927a7a2d
vitalino silva <vito@helu.io>
2023-01-12 16:50:07 +0000

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <header>
+    <nav>
+      <NuxtLink to="/">Ecommerce Nuxt3 Playground</NuxtLink>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- out the page content -->
+  <div>
+    <slot />
+  </div>
+
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+ .router-link-exact-active {
+   color: #12b488;
+ }
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <p>The details page is here with the id: </p>
+    <p>The details page is here with the id: {{ id }}</p>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+const { id } = useRoute().params;
+
 
 </script>
 
