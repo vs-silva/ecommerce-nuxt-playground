c78be077ce99c3150b33a1573a4b395885116507
vitalino silva <vito@helu.io>
2023-01-19 12:34:05 +0000

    added loader and updated test:cov script

--- adapters/rest-client.adapter.ts
+++ adapters/rest-client.adapter.ts
@@ -1,19 +1,20 @@
 import type {ProductDrivenPort} from "~/application/products/ports/product-driven.port";
 import axios from 'axios';
-import 'js-loading-overlay';
+import 'js-loading-overlay' ;
+
 
 export function RestClientAdapter(): ProductDrivenPort {
-
-    // @ts-ignore
-    const loader = JsLoadingOverlay;
-    let isLoaderActive = false;
-    const loaderPool: number[] = [];
 
     const engine = axios.create({
         baseURL: 'https://fakestoreapi.com',
         timeout: ( 60 * 1000 ),
         timeoutErrorMessage: 'Something is wrong with the endpoint'
-    });
+    })
+
+    // @ts-ignore
+    const loader = JsLoadingOverlay;
+    let isLoaderActive = false;
+    const loaderPool: number[] = [];
 
     engine.interceptors.request.use(config => {
         loaderPool.push(1);
@@ -34,12 +35,12 @@ export function RestClientAdapter(): ProductDrivenPort {
         return Promise.reject(error);
     }
 
-    function handleLoader(): void{
+    function handleLoader(): void {
         if(loaderPool.length) {
             if(!isLoaderActive) {
                 loader.show({
                     "overlayBackgroundColor": "#000000",
-                    "spinnerIcon": "ball-newton-cradle",
+                    "spinnerIcon": "square-loader",
                     "overlayOpacity": "0.5",
                     "spinnerColor": "#00BD7E"
                 });
